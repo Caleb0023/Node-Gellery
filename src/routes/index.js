e71d7505bc436d84819b3e13c0c17b797a774f65
Caleb0023 <calebmendez80@gmail.com>
2020-10-07 20:14:27 -0600

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const path = require('path');
+const { unlink } = require('fs-extra');
+const router = Router();
+//model
+const Image = require('../models/Image');
+
+router.get('/', async (req, res) => {
+    const images = await Image.find();
+    res.render('index.ejs', { images });
+});
+
+router.get('/upload', (req, res) => {
+    res.render('upload.ejs');
+})
+
+router.post('/upload', async (req, res) => {
+    const image = new Image();
+    image.title = req.body.title;
+    image.description = req.body.description;
+    image.filename = req.file.filename;
+    image.path = '/uploads/' + req.file.filename;
+    image.originalname = req.file.originalname;
+    image.mimetype = req.file.mimetype;
+    image.size = req.file.size;
+    await image.save();
+    res.redirect('/')
+})
+
+router.get('/image/:id', async (req, res) => {
+    const { id } = req.params;
+    const image = await Image.findById(id);
+    res.render('profile.ejs', { image });
+})
+
+router.get('/image/:id/delete', async (req, res) => {
+    const { id } = req.params;
+    const imageDeleted = await Image.findByIdAndDelete(id);
+    await unlink(path.resolve('./src/public' + imageDeleted.path));
+    res.redirect('/');
+})
+
+module.exports = router;
